204425d82f750db213833b18f3af3e485ada9b00
Rishad1404 <rishadislam2019@gmail.com>
2024-03-04 21:04:33 +0600

    appending the title and count

--- styles/index.js
+++ styles/index.js
@@ -1,30 +1,29 @@
 
-const loadPosts=async (searchText)=>{
-    
-    const res=await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`);
-    const data=await res.json();
-    const posts=data.posts;
+const loadPosts = async (searchText) => {
+
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`);
+    const data = await res.json();
+    const posts = data.posts;
     displayPosts(posts);
 }
 
-const displayPosts=posts=>{
+const displayPosts = posts => {
     // console.log(posts);
 
     // Access to post container div
-    const postContainer=document.getElementById('post-container');
+    const postContainer = document.getElementById('post-container');
 
-    postContainer.textContent=''
-    posts.forEach(post=>{
+    postContainer.textContent = ''
+    posts.forEach(post => {
         // console.log(post)
-        
-        
-        
-        
+
+
+
         // Create the post container div
-        const postCard=document.createElement('div')
-        postCard.classList='lg:flex gap-5 bg-[#797dfc1a] border-1 border-solid border-[#797DFC] p-8 mb-10 ';
+        const postCard = document.createElement('div')
+        postCard.classList = 'lg:flex gap-5 bg-[#797dfc1a] border-1 border-solid border-[#797DFC] p-8 mb-10 ';
         // Set inner html   
-        postCard.innerHTML=`
+        postCard.innerHTML = `
                         <div>
                             <div class="avatar relative">
                                 <div class="w-24 mask mask-squircle">
@@ -37,16 +36,16 @@ const displayPosts=posts=>{
                         </div>
                         <div>
                             <div class="flex gap-5">
-                                <p># ${post.category}</p>
+                                <p>${post.id} ${post.category}</p>
                                 <p>Author: ${post.author.name}</p>
                             </div>
-                            <h3 class="text-xl mt-2 mb-3 font-bold">${post.title}</h3>
+                            <h3 id="postTitle" class="text-xl mt-2 mb-3 font-bold">${post.title}</h3>
                             <p>${post.description}
                             </p>
                 
                             <hr class="border-t border-dashed border-black my-4">
                 
-                            <div class="flex justify-between items-start mt-5 w-auto">
+                            <div class="flex justify-between items-start mt-5 ">
                                 <div class="flex gap-6 items-center">
                                     <div class="flex gap-4 items-center">
                                         <img src="images/tabler-icon-message-2.svg" alt="">
@@ -54,25 +53,25 @@ const displayPosts=posts=>{
                                     </div>
                                     <div class="flex gap-4 items-center">
                                         <img src="images/tabler-icon-eye.svg" alt="">
-                                        <p>${post.view_count}</p>
+                                        <p id="viewCount">${post.view_count}</p>
                                     </div>
                                     <div class="flex gap-4 items-center">
                                         <img src="images/tabler-icon-clock-hour-9.svg" alt="">
                                         <p>${post.posted_time} min</p>
                                     </div>
                                 </div>
-                                <button><img src="images/email 1.svg" alt=""></button>
+                                <button onclick="handleShowDetail('${post.id}');"><img src="images/email 1.svg" alt=""></button>
                             </div>
                         </div>
         `;
         postContainer.appendChild(postCard);
 
         const activeStatusElements = postCard.getElementsByClassName('active-status');
-        const activeStatus = activeStatusElements[0]; 
+        const activeStatus = activeStatusElements[0];
         if (post.isActive) {
             activeStatus.classList.add('bg-green-900');
         } else {
-            
+
             activeStatus.classList.add('bg-red-700');
         }
     })
@@ -80,16 +79,16 @@ const displayPosts=posts=>{
     controlLoadingSpinner(false);
 }
 // Latest posts
-function loadPosts2(){
+function loadPosts2() {
     fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
-    .then(res=>res.json())
-    .then(data=>{
+        .then(res => res.json())
+        .then(data => {
 
-        const latestPostContainer=document.getElementById('latest-container')
-        data.forEach(item=>{
-            const latestPostCard=document.createElement('div');
-            latestPostCard.classList='';
-            latestPostCard.innerHTML=`
+            const latestPostContainer = document.getElementById('latest-container')
+            data.forEach(item => {
+                const latestPostCard = document.createElement('div');
+                latestPostCard.classList = '';
+                latestPostCard.innerHTML = `
             <div class="card w-96 bg-base-100 shadow-xl my-10">
                         <figure class="px-10 pt-10">
                           <img src="${item.cover_image}" alt="" class="rounded-xl" />
@@ -98,7 +97,7 @@ function loadPosts2(){
                         <div class="card-body text-left">
                             <div class="flex items-center gap-4">
                                 <img src="images/Frame (1).svg" alt="">
-                                <p class="">${item.author.posted_date}</p>
+                                <p class="">${item.author?.posted_date??"No Publish Date"}</p>
                             </div>
                           <h2 class="card-title font-bold my-4">${item.title}</h2>
                           <p>${item.description} </p>
@@ -106,39 +105,71 @@ function loadPosts2(){
                             <img src="${item.profile_image}" class="w-12 rounded-full " alt="">
                             <div class=>
                                 <h2 class="font-bold">${item.author.name}</h2>
-                                <p>${item.author.designation}</p>
+                                <p>${item.author?.designation??"Unknown"}</p>
                             </div>
                           </div>
                         </div>
                 </div>
             `;
-            latestPostContainer.appendChild(latestPostCard)
+                latestPostContainer.appendChild(latestPostCard)
+            })
         })
-    })
 }
-    
 
-const displayLatestPosts=data=>{
+const displayLatestPosts = data => {
     console.log(data)
 }
-// Handle search button
-
-const handleSearch=()=>{
-    controlLoadingSpinner(true);
-    const searchField=document.getElementById('search-field');
-    const searchText=searchField.value;
-    console.log(searchText);
-    loadPosts(searchText)
+// Handling texts from posts
+let count=0
+const handleShowDetail = async (id) => {
     
+    
+    
+    const res=await fetch (`https://openapi.programming-hero.com/api/retro-forum/posts?category??${id}`);
+    const data=await res.json();
+    const posts=data.posts;
+    
+    count++;
+    document.getElementById('count-value').innerText=count;
+
+    for(const post of posts){
+        // console.log(post)
+
+        const detailsContainer = document.getElementById('details-container1');
+        const newContainer = document.createElement('div');
+         newContainer.classList = 'bg-slate-100 rounded-lg p-4 flex justify-between gap my-3';
+        newContainer.innerHTML = `
+                        <p>${post.title}</p>
+                            <div class="flex gap-2 items-center justify-evenly">
+                                <img src="images/tabler-icon-eye.svg" alt="">
+                                <p>${post.view_count}</p>
+                            </div>
+    `;
+        detailsContainer.appendChild(newContainer)   
+    }    
 }
 
-const controlLoadingSpinner=(isLoading)=>{
-    const loadingSpinner=document.getElementById('loading-spinner');
-    if(isLoading){
+
+// Handle search button
+
+const handleSearch = () => {
+    controlLoadingSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPosts(searchText)
+
+}
+
+const controlLoadingSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if (isLoading) {
         loadingSpinner.classList.remove('hidden')
     }
-    else{
-        loadingSpinner.classList.add('hidden')
+    else {
+        setTimeout(() => {
+            loadingSpinner.classList.add('hidden');
+        }, 2000);
     }
 }
 
